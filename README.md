466e90379169aa29be0dd548ea248c1066febcbe
Albertov19 <albertoviera@gmail.com>
2022-01-11 11:27:55 +0100

    added moonbeam

--- README.md
+++ README.md
@@ -6,6 +6,29 @@ Based of:
  - https://github.com/gnosis/canonical-weth
  - https://www.npmjs.com/package/canonical-weth
 
+## Install Dependencies
+
+Run:
+
+```
+npm i
+```
+
+## Verifying the Contract
+
+To start you need to export two variables:
+
+ - `ALCHEMY_API` - API Key for Alchemy on Ethereum MainNet (to verify the smart contract)
+ - `PRIV_KEY` - Private Key to deploy contract (not needed to verify)
+
+To verify, run:
+
+```
+npx hardhat run verify-script.js
+```
+
 ## Deployed Contract Address
 
- - WMOVR: `0xf50225a84382c74CbdeA10b0c176f71fc3DE0C4d` 
+ - WMOVR (on Moonriver): `0xf50225a84382c74CbdeA10b0c176f71fc3DE0C4d`
+ - WGLMR (on Moonbeam):  `0xf50225a84382c74CbdeA10b0c176f71fc3DE0C4d` 
+
--- hardhat.config.js
+++ hardhat.config.js
@@ -8,46 +8,52 @@ require('@nomiclabs/hardhat-ethers');
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  // Networks
-  networks: {
-    hardhat: {
-      forking: {
-        url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API}`,
-      },
-    },
-    moonbase: {
-      url: 'https://rpc.testnet.moonbeam.network',
-      accounts: [process.env.PRIV_KEY],
-      gasPrice: 1000000000,
-      chainId: 1287,
-    },
-    moonriver: {
-      url: 'https://rpc.moonriver.moonbeam.network',
-      accounts: [process.env.PRIV_KEY],
-      gasPrice: 1000000000,
-      chainId: 1285,
-    },
-  },
-
-  // Solc
-  solidity: {
-    compilers: [
-      {
-        version: '0.4.19',
-        settings: {
-          optimizer: {
-            enabled: false,
-            runs: 200,
-          },
+    // Networks
+    networks: {
+        hardhat: {
+            forking: {
+                url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API}`,
+            },
         },
-      },
-    ],
-  },
+        moonbase: {
+            url: 'https://rpc.api.moonbase.moonbeam.network',
+            accounts: [process.env.PRIV_KEY],
+            gasPrice: 1000000000,
+            chainId: 1287,
+        },
+        moonriver: {
+            url: 'https://rpc.moonriver.moonbeam.network',
+            accounts: [process.env.PRIV_KEY],
+            gasPrice: 1000000000,
+            chainId: 1285,
+        },
+        moonbeam: {
+            url: 'https://rpc.api.moonbeam.network',
+            accounts: [process.env.PRIV_KEY],
+            gasPrice: 100000000000,
+            chainId: 1284,
+        },
+    },
 
-  // Paths
-  paths: {
-    sources: './contracts',
-    cache: './cache',
-    artifacts: './artifacts',
-  },
+    // Solc
+    solidity: {
+        compilers: [
+            {
+                version: '0.4.19',
+                settings: {
+                    optimizer: {
+                        enabled: false,
+                        runs: 200,
+                    },
+                },
+            },
+        ],
+    },
+
+    // Paths
+    paths: {
+        sources: './contracts',
+        cache: './cache',
+        artifacts: './artifacts',
+    },
 };
